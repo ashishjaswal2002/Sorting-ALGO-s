5816674e11193837ade8419d67b198e0867a9dc9
Ashish jaswal <jaswalashish131@gmail.com>
2022-11-23 23:17:22 +0530

    Quick Sort Using Lomuto Partition

--- Quick Sort/Lomuto_Part.java
+++ Quick Sort/Lomuto_Part.java
@@ -4,32 +4,41 @@ public class Lomuto_Part {
      int arr[] = {10,80,30,90,40,50,70};
      int n = arr.length;
    
-     lomutoPartition(arr, n);
+    QuickSort(arr, 0, n-1);
      for(int i=0;i<n;i++){
         System.out.print(arr[i]+" ");
      }
     }
-    static void  lomutoPartition(int arr[],int n){
-        int l = 0;
-        int h = n-1;
-        int pivot = arr[h];
-        
-        
-         
-         
 
-        int i = l-1;
+    static int    lomutoPartition(int arr[],int l,int h){
+
+        int i =l-1;
+        int pivot = arr[h];
+
         for(int j=l;j<=h-1;j++){
-          if(arr[j]<pivot){
+           if(arr[j]<pivot){
             i++;
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
-          }
-        }
-       int temp = arr[i+1];
-       arr[i+1] = arr[h];
-       arr[h] = temp;
+            
+           }
 
+        }
+        int temp  = arr[i+1];
+        arr[i+1]= arr[h];
+        arr[h] = temp;
+  
+        return i+1;
+
+    }
+    static void QuickSort(int arr[],int l,int h){
+     if(l<h){
+      int p = lomutoPartition(arr, l, h);
+      QuickSort(arr, l, p-1);
+      QuickSort(arr, p+1, h);
+     }
+
+      
     }
 }
