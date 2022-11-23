9e77eb4f2fb5c38a11669fb3981780a5e9d57ccb
Ashish jaswal <jaswalashish131@gmail.com>
2022-11-23 23:20:53 +0530

    Adding Horare partition

---
+++ Quick Sort/HorarePartition.java
@@ -0,0 +1,8 @@
+public class HorarePartition {
+    //Quick Sort Can also be done using this Partition.
+
+    public static void main(String[] args) {
+        int arr[] = {2,1,5};
+        
+    }
+}
